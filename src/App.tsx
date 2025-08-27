c910cf5c364ab5230d687070d8014b1b00d354dd
Toan <nguyentoan04.0003@gmail.com>
2025-08-27 17:06:30 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { AccountProvider } from "./providers/userProvider";
-import AppRouter from "./routers/AppRouter";
+import { AccountProvider } from "@/providers/userProvider";
+import AppRouter from "@/routers/AppRouter";
 
 
 export default function App() {
